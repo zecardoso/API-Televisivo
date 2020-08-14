e0002ebb5b8cba96e8c082378e518c5b946b5f3f
José Cardoso <zehcardoso@outlook.com>
2020-08-14 01:45:51 -0300

    comentando variavel publicacao

--- src/main/java/com/televisivo/model/Episodio.java
+++ src/main/java/com/televisivo/model/Episodio.java
@@ -2,7 +2,6 @@ package com.televisivo.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -17,13 +16,9 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -66,10 +61,10 @@ public class Episodio implements Serializable {
 	@Column(nullable = false)
     private int duracao;
 
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(nullable = false, columnDefinition = "DATE")
-    private Date publicacao;
+    // @DateTimeFormat(pattern = "dd/MM/yyyy")
+    // @Temporal(TemporalType.TIMESTAMP)
+    // @Column(nullable = false, columnDefinition = "DATE")
+    // private Date publicacao;
 
     @ManyToOne(targetEntity = Temporada.class, fetch = FetchType.LAZY)
 	@JoinColumn(name = "temporada_id", nullable = false, referencedColumnName = "temporada_id", foreignKey = @ForeignKey(name = "FK_TEMPORADA_EPISODIO"))
